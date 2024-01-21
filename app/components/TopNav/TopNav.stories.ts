6043174f3b5b743cc0d9c52b496aa1f1bebe1c40
Sunjay Armstead <connect@sunjayarmstead.local>
2024-01-21 14:49:13 -0500

    Remove unneeded keys from TopNav stories

--- app/components/TopNav/TopNav.stories.ts
+++ app/components/TopNav/TopNav.stories.ts
@@ -3,11 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import TopNaV from "./TopNav";
 
 const meta = {
-  title: "Components/TopNav",
   component: TopNaV,
-  parameters: {
-    backgrounds: { default: "dark" },
-  },
 } satisfies Meta<typeof TopNaV>;
 
 export default meta;

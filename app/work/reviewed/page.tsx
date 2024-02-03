c61aac1e247c9a9ff8814862800621c4645a2fb7
Sunjay Armstead <connect@sunjayarmstead.local>
2024-02-03 18:48:13 -0500

    Add first copy section with problem and start of solution

--- app/work/reviewed/page.tsx
+++ app/work/reviewed/page.tsx
@@ -16,6 +16,40 @@ export default function CaseStudyReviewed() {
             go to Reviewed to get fair product comparisons backed by scientific
             research. Look no further for nana’s next smart photo frame."
       />
+      <section className="mx-auto max-w-[954px] pt-16 px-5 md:px-10 lg:px-0">
+        <h2 className="text-3xl font-serif text-black dark:text-white mb-2">
+          The problem
+        </h2>
+        <p className="text-base text-black dark:text-white mb-8">
+          Reviewed, as part of the USA TODAY network, was growing rapidly and
+          their team needed a single place for users to sign up for an expanding
+          list of email newsletters.
+        </p>
+        <h2 className="text-3xl font-serif text-black dark:text-white mb-2">
+          The solution
+        </h2>
+        <p className="text-base text-black dark:text-white mb-2">
+          There are several factors to consider when addressing a problem like
+          Reviewed was facing. First up, who are the users we’re solving for? In
+          this case, Reviewed needed a solution that delighted their customers
+          and would be easy for their team to manage. That’s two primary user
+          groups.
+        </p>
+        <p className="text-base text-black dark:text-white mb-2">
+          Next, it’s important to consider the unique needs of each user.
+          Customers need a simple way to enter in their contact information and
+          select the newsletters they want to subscribe to. However, Reviewed’s
+          administrative team was less concerned about a streamlined newsletter
+          signup process. Instead, they needed a simple way to manage their
+          subscriptions and add new newsletter options in the future.
+        </p>
+        <p className="text-base text-black dark:text-white">
+          Finally, any addition to an existing user interface should match
+          established design patterns. At the same time, it is my job as a
+          designer to push the envelope. In this way, my aim for Reviewed was to
+          achieve an appropriate balance of innovation and continuity.
+        </p>
+      </section>
     </>
   );
 }

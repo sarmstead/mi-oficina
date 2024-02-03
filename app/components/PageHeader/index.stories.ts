102a44b66a80b64995e518753b924b005c73e57d
Sunjay Armstead <connect@sunjayarmstead.local>
2024-02-03 18:38:15 -0500

    Add page header component and story

---
+++ app/components/PageHeader/index.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import PageHeader from "~components/PageHeader";
+
+const meta = {
+  component: PageHeader,
+  argTypes: {
+    lightBackground: {
+      control: "text",
+      description: "Tailwind class for light mode",
+    },
+    darkBackground: {
+      control: "text",
+      description: "Tailwind class for dark mode",
+    },
+    title: {
+      control: "text",
+      description: "Title of header",
+    },
+    subtitle: {
+      control: "text",
+      description: "Subtitle of header",
+    },
+  },
+  args: {
+    lightBackground: "bg-black",
+    darkBackground: "dark:bg-prettyDark",
+    title: "A case study: Lorem Ipsum",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof PageHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
--- app/work/reviewed/page.tsx
+++ app/work/reviewed/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import PageHeader from "~/components/PageHeader";
 
 export const metadata: Metadata = {
   title: "Sunjay Armstead | Reviewed Case Study",
@@ -9,18 +10,12 @@ export const metadata: Metadata = {
 export default function CaseStudyReviewed() {
   return (
     <>
-      <header className="min-h-[350px] bg-black dark:bg-prettyDark flex flex-col justify-center px-5 md:px-10 lg:px-0">
-        <div className="w-full max-w-[954px] mx-auto flex flex-col gap-4">
-          <h1 className="font-serif text-white text-4xl lg:text-5xl">
-            A case study: Reviewed
-          </h1>
-          <p className="text-base text-white w-full max-w-[589px]">
-            Reviewed is your go-to place for all things, well, reviewed. People
+      <PageHeader
+        title="A case study: Reviewed"
+        subtitle="Reviewed is your go-to place for all things, well, reviewed. People
             go to Reviewed to get fair product comparisons backed by scientific
-            research. Look no further for nana’s next smart photo frame.
-          </p>
-        </div>
-      </header>
+            research. Look no further for nana’s next smart photo frame."
+      />
     </>
   );
 }

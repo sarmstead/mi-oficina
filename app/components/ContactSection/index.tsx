89ebc3124c41ff01d3606b11d3e5859c1c48d450
Sunjay Armstead <connect@sunjayarmstead.local>
2024-02-03 17:40:50 -0500

    Add reviewed route. Add work layout.

---
+++ app/components/ContactSection/index.tsx
@@ -0,0 +1,9 @@
+import ContactForm from "~components/ContactForm/ContactForm";
+
+const ContactSection = () => (
+  <section className="px-5 pt-12 pb-24 lg:pt-16 lg:px-0 max-w-[954px] m-auto">
+    <ContactForm />
+  </section>
+);
+
+export default ContactSection;
---
+++ app/work/reviewed/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sunjay Armstead | Reviewed Case Study",
+  description:
+    "Reviewed was growing rapidly and needed a single place for newsletter signups. I tackled this problem with intuitive design and thoughtful engineering.",
+};
+
+export default function CaseStudyReviewed() {
+  return <></>;
+}

40354bbb342b94f81e824122ec82b8c07daf1205
Sunjay Armstead <connect@sunjayarmstead.local>
2024-02-14 21:42:42 -0500

    Add creatmoteo dynamic route

---
+++ app/(blog)/creatmoteo/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import { getArticleBySlug, getAllSlugs } from "~/(blog)/sanity-actions";
+
+import PageHeader from "~components/PageHeader";
+
+export default async function Article({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const article = await getArticleBySlug(params.slug, "Creat-Moteo");
+  const date = new Date(article.publishDate).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const authors = article.authors.map(
+    (author: { firstName: string; lastName: string }) => {
+      return `${author.firstName} ${author.lastName}`;
+    },
+  );
+  const title = article.title;
+
+  return (
+    <>
+      <PageHeader title={title} subtitle={`${authors.join(" ")} | ${date}`} />
+      <article
+        className="article mx-auto max-w-innerContainer py-16 px-5 md:px-10 lg:px-0"
+        dangerouslySetInnerHTML={{ __html: article.body }}
+      ></article>
+    </>
+  );
+}
+
+export async function generateStaticParams() {
+  const slugs = await getAllSlugs("Creat-Moteo");
+
+  return slugs.map((slug: { _type: "slug"; current: string }) => ({
+    slug: slug.current,
+  }));
+}
